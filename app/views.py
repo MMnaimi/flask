ae6cc25f6f9a80cd70ed85141e9fbac12df27bd3
MMnaimi <mm.naimi@yahoo.com>
2022-04-16 16:18:50 +0430

    working with simple static file (css, js, img) using url_for with jinja

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from app import app
+
+from flask import render_template
 @app.route("/")
 def index():
-    return "the value of __name__ is ={}".format(__name__)
+    return render_template("public/index.html")
 @app.route("/about")
 def about():
     return("<h1 style='color:red'>About!!!!!</h1>")
